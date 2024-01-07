2834d18f5374e999d262928fbfc4d48e0b9b0d75
Harrybeaty <1beatyh@gmail.com>
2024-01-07 18:07:59 +0000

    Printed names and birthdates in one for loop.

--- 14_IO_input/dob_task.py
+++ 14_IO_input/dob_task.py
@@ -9,12 +9,14 @@ with open('DOB.txt', 'r+') as file:         # Open DOB.txt in read and write mod
         birthdate = ' '.join(words[2:])     # Join the rest of the elements (person's birthdate) with a space.
         name_list.append(name)              # Add each name to name_list.
         birthdate_list.append(birthdate)    # Add each birthdate birthdate_list.
+        print(name)
+        print(birthdate)
 
-print("Name")                               # Display list title.
+""" print("Name")                               # Display list title.
 for person_name in name_list:               # For each person in name list print their name.
     print(person_name)                      
 print('\n')                                 # Print space between displayed lists
 print("Birthdate")                          # Display list title
 for person_birthdate in birthdate_list:     # For eaxh person in birthdate_list print their birthdate.    
-    print(person_birthdate.strip())         # Strip white space at the end because it was printing a extra line inbetween each date.
+    print(person_birthdate.strip())     """     # Strip white space at the end because it was printing a extra line inbetween each date.
     
